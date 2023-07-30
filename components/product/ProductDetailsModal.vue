c13b6ca5ec7537cd1c87aba1f48e8d25ec1bbe29
Mahfuz Ahmed <m4mahfuz@gmail.com>
2023-07-30 20:12:23 +0600

    modified

--- components/product/ProductDetailsModal.vue
+++ components/product/ProductDetailsModal.vue
@@ -2,13 +2,14 @@
   <div v-if="modal" class="fixed w-full h-full min-h-screen z-30 top-0 left-0">
       <div @click.prevent="modalClose" class="absolute bg-black opacity-60 h-full w-full top-0 left-0"></div>
       <!-- <overlay-scrollbars :key="scroll" class="h-full">  -->
-      <div class="lg:bs-modal-body bg-slate-100 rounded-2xl p-8 mx-4 lg:mx-10 my-10 z-30 relative h-4/5 overflow-y-auto">
+      <!-- <div class="lg:bs-modal-body bg-slate-100 rounded-2xl p-8 mx-4 lg:mx-10 my-10 z-30 relative h-4/5 overflow-y-auto"> -->
+      <div class="lg:bs-modal-body bg-slate-100 rounded-2xl p-8 mx-4 lg:mx-10 my-10 z-30 relative min-h-max overflow-y-auto">
         <svg @click.prevent="modalClose" xmlns="http://www.w3.org/2000/svg" class="h-4 w-5 absolute right-6 top-5 cursor-pointer hover:text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
 
-        <div class="flex justify-center flex-wrap lg:flex-nowrap my-2">
-          <div class="min-w-full w-80 lg:min-w-0 px-8">
+        <div class="lg:flex justify-center my-4 lg:mx-4">
+          <div class="w-full md:px-8">
             <ProductZoomer 
               :images="images" 
               :pane="pane" 
@@ -16,9 +17,9 @@
               :slides="3"
             />
           </div>
-          <div class="drift-pane relative px-4 md:px-8 text-left w-full">
+          <div class="drift-pane relative md:px-8 text-left w-full">
             <p class="text-sm mb-3"><span class="uppercase text-gray-400 pr-6">Status</span> <span class="bs-dark-green-color">In Stock</span></p>
-            <h3 class="text-2xl">{{product.name}}</h3>
+            <h3 class="text-lg md:text-2xl">{{product.name}}</h3>
             <p class="text-xs text-gray-400 mb-4 mt-2"><b>{{product.quantity}}</b> items available.</p>
             <p class="text-2xl font-bold">
               <span v-if="product.price">
@@ -29,19 +30,19 @@
               </span>
               <span v-else><HelperTkSymbol /> {{ product.original_price }}</span>
             </p>
-            <div class="flex flex-col items-center justify-center border-t border-gray-200 my-4">              
+            <div class="flex flex-col items-start justify-center border-t border-gray-200 my-4">              
               <div class="md:flex justify-center items-center text-4xl text-gray-600 my-4 gap-2 p-0">
                 <span class="pl-2 text-lg uppercase">quantity</span>
                 <div class="flex gap-2 md:gap-6 md:w-64">
-                  <button @click.prevent="emitEventForCartOperaton('decrease')" class="h-14 w-14 border border-gray-200 rounded-full" :disabled="cartProductQuantity < 1">-</button>
+                  <button @click.prevent="emitEventForCartOperaton('decrease')" class="h-7 w-7 md:h-14 md:w-14 border border-gray-200 rounded-full" :disabled="cartProductQuantity < 1">-</button>
                   <span class="flex items-center justify-center w-14 h-14 border border-gray-200 bg-gray-200 rounded-full text-xl font-semibold">{{cartProductQuantity}}</span>
-                  <button @click.prevent="emitEventForCartOperaton('increase')" class="h-14 w-14 border border-gray-200 rounded-full">+</button>
+                  <button @click.prevent="emitEventForCartOperaton('increase')" class="h-7 w-7 md:h-14 md:w-14 border border-gray-200 rounded-full">+</button>
                 </div>
               </div>
-              <div class="py-2 w-full">
+              <div class="py-2 w-full xl:w-3/4">
                 <button class="bg-green-500 text-white hover:bg-green-600 hover:text-gray-200 rounded-full w-full px-2 py-2 font-medium">View Cart</button>
               </div>
-              <div class="py-2 w-full">
+              <div class="py-2 w-full xl:w-3/4">
                 <button class="bg-orange-500 text-gray-800 hover:bg-orange-600 hover:text-gray-900 rounded-full w-full px-2 py-2 font-medium">Checkout</button>
               </div>
             </div>
